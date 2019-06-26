90c73274510114ecf4452c18d7de34aec840521b
SlupikB <bartek.slupik@silvair.com>
2019-06-26 15:53:25 +0200

    note

--- img_to_ascii.py
+++ img_to_ascii.py
@@ -13,6 +13,8 @@ sunset.jpg: func=IACurves.shift(-20), resize=0.06
 you_are_free.jpg: func=IACurves.CONTRAST_L, resize=0.05
 
 Feel free to experiment with different settings and images!
+
+Set console font size to something like 7 or resize even further!
 """
 
 imgAscii = ImageAsciifier(func=IACurves.CONTRAST_M)
