607411666ed67d0db0f62b237785159cc44370ca
SlupikB <bartek.slupik@silvair.com>
2019-06-27 13:51:30 +0200

    Added an example script for VideoAsciifier

---
+++ camera_preview.py
@@ -0,0 +1,21 @@
+##############################################
+# Live camera preview converted to ASCII Art #
+##############################################
+
+from asciify import VideoAsciifier, IACurves
+
+"""
+OPTIMAL PARAMETERS:
+func=IACurves.CONTRAST_L
+resize depends on camera resolution, in my case 0.3
+fps=30 works fine and smooth
+For larger sizes the preview may glitch, try lowering resize
+
+Feel free to experiment with different settings and images!
+
+Set console font size to something like 7 or resize even further down!
+"""
+
+vidAscii = VideoAsciifier(func=IACurves.CONTRAST_L)
+vidAscii.start_preview(fps=30, resize=0.25)
+# exit using CTRL+C
--- img_to_ascii.py
+++ img_to_ascii.py
@@ -2,7 +2,7 @@
 # A basic example which converts an image to ASCII Art #
 ########################################################
 
-from asciify import *
+from asciify import ImageAsciifier, IACurves
 
 p = "./images/you_are_free.jpg"  # Enter image path here
 
@@ -14,9 +14,8 @@ you_are_free.jpg: func=IACurves.CONTRAST_L, resize=0.05
 
 Feel free to experiment with different settings and images!
 
-Set console font size to something like 7 or resize even further!
+Set console font size to something like 7 or resize even further down!
 """
 
 imgAscii = ImageAsciifier(func=IACurves.CONTRAST_M)
-imgAscii.img_to_ascii(path=p, resize=0.05, draw=True)
-
+imgAscii.img_to_ascii(load=p, resize=0.05, draw=True)
